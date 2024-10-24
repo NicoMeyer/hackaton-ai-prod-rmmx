58af6f06f57046162696952d4624b85d66ed463a
Diego Moya <diegomp896@gmail.com>
2024-10-24 17:07:35 -0300

    Revert "Revert "Revert "Revert "Revert "feat: pr erroneo"""""

--- app/models/liquidation.rb
+++
@@ -1,9 +0,0 @@
-class Liquidation < ApplicationRecord
-  validates :employee_name, presence: true
-  validates :base_salary, presence: true, numericality: { greater_than: 0 }
-
-  # Error: Suma incorrecta entre decimal y string
-  def total_salary
-    base_salary + bonus_amount
-  end
-end
